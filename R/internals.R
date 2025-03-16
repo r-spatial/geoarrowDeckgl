28c9a42f1a3301b09ca0c1f5f6d9201c4746490e
Tim Appelhans <tim.appelhans@addium.io>
2025-03-16 11:28:40 +0100

    no luck with sfc, wkt and wkb so far...

--- R/internals.R
+++ R/internals.R
@@ -3,6 +3,8 @@ writeInterleavedGeoarrow = function(data, layerId, geom_column_name) {
   dir.create(layer_path)
   layer_path = paste0(layer_path, "/", layerId, "_layer.arrow")
 
+  # FIXME: make work also for wk::wkt and wk::wkb, as well as sfc
+
   geom_type = geoarrow::infer_geoarrow_schema(data, coord_type = "INTERLEAVED")
   data_schema = nanoarrow::infer_nanoarrow_schema(data)
   data_schema$children[[geom_column_name]] = geom_type
